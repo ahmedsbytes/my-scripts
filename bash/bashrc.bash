aae46a03fa04f595502d3b2dd6dd4ab7bd819c95
Ahmed Abdou <mail@ahmd.io>
2020-01-06 00:55:19 +0000

    Update bashrc.bash

--- bash/bashrc.bash
+++ bash/bashrc.bash
@@ -5,3 +5,11 @@ HISTSIZE=5000
 HISTFILESIZE=10000
 shopt -s histappend
 PROMPT_COMMAND='$PROMPT_COMMAND; history -a'
+
+#minikube
+source <(minikube completion bash)
+#kubectl
+source <(kubectl completion bash)
+#https://raw.githubusercontent.com/ahmetb/kubectl-aliases/master/.kubectl_aliases
+source $HOME/.bashrc.d/kubectl_aliases
+complete -o default -F __start_kubectl k
